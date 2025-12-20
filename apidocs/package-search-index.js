619f1926d02007c35f177abd7d3e8d7ca8164376
microbean <ci@microbean.org>
2025-12-20 07:34:22 +0000

    Site checkin for project microBean™ Scopelet

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.microbean.scopelet","l":"org.microbean.scopelet"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"org.microbean.scopelet","l":"org.microbean.scopelet"}];updateSearchResults();
